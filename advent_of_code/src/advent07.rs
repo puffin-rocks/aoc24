8b32b704140f779b5c32280783466e416fcf509a
Maria Putintseva <maria.putintseva@robeco.com>
2024-12-07 13:06:24 +0100

    AoC24.d7.p2 + refactoring

--- advent_of_code/src/advent07.rs
+++ advent_of_code/src/advent07.rs
@@ -2,6 +2,45 @@ use std::collections::{BTreeMap};
 use crate::utils::{Solve, Label, no_solution_message};
 use rayon::prelude::*;
 
+enum Convertable {
+    Binary((usize, usize)),
+    Trinary((usize, usize)),
+}
+
+impl Convertable {
+    fn update_input(&mut self, other :usize){
+        match self {
+            Convertable::Binary((num, _)) | Convertable::Trinary((num, _)) => *num = other
+        }
+    }
+    fn convert(&self)->String{
+        match self{
+            Convertable::Binary((num, width)) => {
+                format!("{:0width$b}", num, width = width)
+            }
+            Convertable::Trinary((num, width)) => {
+                let mut num = *num;
+                if num == 0 {
+                    return format!("{:0>width$}", "0", width = width);
+                }
+                let mut trinary_str = String::new();
+                while num > 0 {
+                    let remainder = num % 3;
+                    trinary_str.push_str(&remainder.to_string());
+                    num /= 3;
+                }
+                format!("{:0>width$}", trinary_str.chars().rev().collect::<String>(), width = width)
+            }
+        }
+    }
+    fn max_operation_code(&self)-> usize{
+        match self {
+            Convertable::Binary((_, width)) => 2_usize.pow(*width as u32),
+            Convertable::Trinary((_, width)) => 3_usize.pow(*width as u32)
+        }
+    }
+}
+
 struct Equation{
     lhs: usize,
     rhs: Vec<usize>
@@ -15,7 +54,7 @@ impl Equation {
         }
     }
 
-    fn try_solve(&self) -> bool {
+    fn try_solve(&self, convertable: fn((usize, usize))->Convertable) -> bool {
         if self.rhs.is_empty() {
             return false;
         }
@@ -25,16 +64,20 @@ impl Equation {
             return self.lhs == self.rhs[0];
         }
 
-        let max_operation_code = 2_usize.pow(n as u32);
-        for operation_code in 0..max_operation_code {
+        let mut c: Convertable = convertable((0, n));
+        for operation_code in 0..c.max_operation_code() {
+            c.update_input(operation_code);
             let mut result = self.rhs[0];
-            let operation_flags = format!("{:0width$b}", operation_code, width = n);
-
-            for (v, op) in self.rhs.iter().skip(1).zip(operation_flags.chars().map(|c| c == '1')) {
-                if op {
-                    result *= v;
-                } else {
-                    result += v;
+            for (v, op) in self.rhs.iter().skip(1).zip(c.convert().chars()) {
+                match op{
+                    '2' => {
+                        let p: u32 = (*v as f64).log(10.0).floor() as u32 + 1;
+                        result*=10_usize.pow(p);
+                        result+=v;
+                    }
+                    '1' => result *= v,
+                    '0' => result += v,
+                    _ => {}
                 }
 
                 if result > self.lhs {
@@ -66,6 +109,35 @@ impl Default for Advent {
     }
 }
 
+impl Advent{
+    fn solve(&self, convertable: fn((usize, usize))->Convertable,
+             result_test: usize,
+             result_prd: usize,
+             verbose: bool,
+             test_mode: bool,
+             part: u8) -> bool{
+
+        if !self.label.has_input { return no_solution_message(verbose, part) }
+
+        let result = self.equations
+            .par_iter()
+            .filter(|&e| {
+                e.try_solve(convertable)
+            })
+            .map(|e|{e.lhs})
+            .sum::<usize>();
+
+        assert_eq!(result, match test_mode{
+            true =>  result_test,
+            false => result_prd
+        });
+        if verbose {
+            println!("Sum of solvable equations: {}", result);
+        }
+        true
+    }
+}
+
 impl Solve for Advent {
     fn get_label(&self) -> &Label{ &self.label }
     fn get_label_mut(&mut self) -> &mut Label {&mut self.label}
@@ -84,13 +156,13 @@ impl Solve for Advent {
 
     fn info(&self){
         if !self.label.has_input {println!("Advent is missing input")}
-        println!("Number of equations {}:", self.equations.len());
+        println!("Number of equations: {}", self.equations.len());
         let mut count = BTreeMap::new();
         self.equations.iter().for_each(|e| *count.entry(e.rhs.len()).or_insert(0) += 1);
 
         for (key, value) in count {
             println!(
-                "Number of equations with {} argument{}: {}",
+                "\tNumber of equations with {} argument{}: {}",
                 key,
                 if key > 1 { "s" } else { "" },
                 value
@@ -99,43 +171,23 @@ impl Solve for Advent {
     }
 
     fn compute_part1_answer(&self, verbose: bool, test_mode: bool) -> bool{
-        if !self.label.has_input { return no_solution_message(verbose, 1) }
-
-        let result: usize = self.equations
-            .par_iter()
-            .filter(|&e| {
-                e.try_solve()
-            })
-            .map(|e|{e.lhs})
-            .sum();
-
-        assert_eq!(result, match test_mode{
-            true =>  3749,
-            false => 1582598718861
-        });
-        if verbose {
-            println!("Sum of solvable equations: {}", result);
-        }
-        true
-        // let mut count = BTreeMap::new();
-        // self.equations.iter().for_each(|e| *count.entry(e.rhs.len()).or_insert(0) += e.is_feasible() as usize);
-        //
-        // for (key, value) in count {
-        //     println!(
-        //         "Number of feasible equations with {} argument{}: {}",
-        //         key,
-        //         if key > 1 { "s" } else { "" },
-        //         value
-        //     );
-        // }
-        // let num: usize = 2_usize.pow(4)-1;
-        // let binary_str = format!("{:b}", num);
-        // println!("Binary as string: {:0>6}", binary_str);
-
+        self.solve(Convertable::Binary,
+                   3749,
+                   1582598718861,
+                   verbose,
+                   test_mode,
+                   1
+        )
     }
-    //
-    // fn compute_part2_answer(&self, verbose: bool, test_mode: bool) -> bool{
-    //     if !self.label.has_input  { return no_solution_message(verbose, 2) }
-    //     false
-    // }
+    fn compute_part2_answer(&self, verbose: bool, test_mode: bool) -> bool{
+        self.solve(Convertable::Trinary,
+                   11387,
+                   165278151522644,
+                   verbose,
+                   test_mode,
+                   2
+        )
+    }
 }
+
+
