275c639a33088cac3bac99cdbe2cce7c16171c82
Maria Putintseva <maria.putintseva@robeco.com>
2024-12-17 11:42:22 +0100

    still cannot make my idea work, added solution of Marco B as benchmark to look at it later

--- advent_of_code/src/advent16.rs
+++ advent_of_code/src/advent16.rs
@@ -1,10 +1,16 @@
 use std::cmp::Ordering;
-use std::collections::BTreeSet;
-use crate::geometry::{CanvasAsync, Direction, Point2D};
+use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet, VecDeque};
+use crate::geometry::{CanvasAsync, Direction, Point2D, Vector};
 use crate::utils::{Solve, Label, assert_display};
 
 use rayon::prelude::*;
 use std::sync::{Arc, Mutex};
+
+#[derive(Debug, Clone, PartialEq)]
+struct PositionPath{
+    position: Vector,
+    path: HashSet<Arc<Point2D>>
+}
 
 #[derive(Debug, Clone, PartialEq)]
 struct ScoredPosition{
@@ -75,6 +81,7 @@ impl Solve for Advent {
     }
     fn compute_part1_answer(&self, test_mode: bool) -> Result<String, String>{
         self.check_input(Some(1))?;
+        return  Err(String::from("Skip Part 1"));
         let start = self.canvas.try_locate_element(&'S')?;
         let finish = self.canvas.try_locate_element(&'E')?;
         let obstacles = self.canvas.try_locate_element(&'#')?;
@@ -132,8 +139,13 @@ impl Solve for Advent {
             let mut queue: BTreeSet<ScoredPosition> = BTreeSet::new();
             queue.insert(ScoredPosition::new(0, Direction::Right, start.first().unwrap().clone()));
             let mut min_score: Option<usize> = None;
+            let mut threshold: usize = 1_000;
             loop{
                 if let Some(p) = queue.pop_first(){
+                    if p.score> threshold {
+                        println!("{:?}", (&p.score));
+                        threshold+=1_000;
+                    }
                     if p.location == *finish_p{
                         min_score = Some(p.score);
                         break;
@@ -162,8 +174,208 @@ impl Solve for Advent {
             Err(String::from("Multiple start or end locations"))
         }
     }
-    // fn compute_part2_answer(&self, test_mode: bool) -> Result<String, String>{
-    //     self.check_input(Some(2))?;
-    //     Err(String::from("Not solved yet"))
-    // }
+    fn compute_part2_answer(&self, test_mode: bool) -> Result<String, String>{
+        self.check_input(Some(2))?;
+        let start = self.canvas.try_locate_element(&'S')?;
+        let finish = self.canvas.try_locate_element(&'E')?;
+        let obstacles = self.canvas.try_locate_element(&'#')?;
+        if start.len() == 1 && finish.len() == 1 {
+
+            //Marco B
+            // let elements = self.canvas.elements();
+            // let mut map0: HashMap<(isize, isize), char> = HashMap::new();
+            // for (ch, points) in elements.iter(){
+            //     let chout = if (**ch == '#'){
+            //         '#'
+            //     }else{
+            //         '.'
+            //     };
+            //     for p in points{
+            //         let tmp = **p;
+            //         map0.insert((*tmp.x(), *tmp.y()), chout);
+            //     }
+            // }
+            // let f_p = **finish.first().unwrap();
+            // let s_p = **start.first().unwrap();
+            //
+            // let (_, paths) = traverse(&map0, &(*s_p.x(), *s_p.y()), &(*f_p.x(), *f_p.y()));
+            // println!("{}", paths.into_iter().collect::<HashSet<(isize, isize)>>().len());
+
+
+            let finish_p = finish.first().unwrap();
+            let start_p = start.first().unwrap();
+
+            let v = Vector::new(Direction::Left, (**start_p).clone());
+            let mut path: HashSet<Arc<Point2D>> = HashSet::new();
+            path.insert(start_p.clone());
+
+            let mut visited: HashMap<Vector, usize> = HashMap::new();
+            visited.insert(v.clone(), 0);
+
+
+            type PathsToPoint = HashMap<Arc<Point2D>, Vec<PositionPath>>;
+            let mut ptp: PathsToPoint = HashMap::new();
+            let pp = PositionPath{ position: v, path };
+            ptp.insert(start_p.clone(), vec![pp]);
+
+            let mut queue: BTreeMap<usize, PathsToPoint> = BTreeMap::new();
+            queue.insert(0, ptp);
+            let mut min_score: Option<usize> = None;
+            let mut threshold: usize = 1_000;
+
+            loop {
+                if let Some((p_score, map)) = queue.pop_first() {
+                    if p_score > threshold{
+                        println!("{:?}", &p_score);
+                        threshold += 1_000;
+                    }
+                    if let Some(paths_to_finish) = map.get(finish_p){
+                        min_score = Some(p_score);
+                        println!("S {:?}", p_score);
+                        let mut points: HashSet<Arc<Point2D>> = HashSet::new();
+                        for p in paths_to_finish.iter(){
+                            points.extend(p.path.iter().cloned());
+                        }
+                        println!("N {:?}", points.len());
+                        break;
+                    }
+                    for (_, paths) in map.iter() {
+                        for p in paths.iter(){
+                            let next_dirs = match p.position.direction() {
+                                Direction::Up | Direction::Down => [*p.position.direction(), Direction::Left, Direction::Right],
+                                Direction::Right | Direction::Left => [*p.position.direction(), Direction::Up, Direction::Down],
+                                _ => unreachable!()
+                            };
+                            for d in next_dirs.iter() {
+                                let next_p = Arc::new(p.position.anchor() + d);
+                                let next_v = Vector::new(d.clone(), *next_p);
+
+                                if !obstacles.contains(&next_p) {
+                                    let score: usize = if d == p.position.direction() { 1 } else { 1001 };
+                                    let next_score = p_score + score;
+                                    let skip = match visited.get(&next_v) {
+                                        Some(s) => *s < next_score,
+                                        None => false
+                                    };
+                                    if !skip {
+                                        let mut next_path: HashSet<Arc<Point2D>> = p.path.iter().cloned().collect();
+                                        next_path.insert(next_p.clone());
+                                        let next_pp = PositionPath{ position: next_v.clone(), path: next_path};
+                                        visited.insert(next_v.clone(), next_score);
+
+                                        if let Some(ptp) = queue.get_mut(&next_score) {
+                                            if let Some(other_path) = ptp.get_mut(&next_p) {
+                                                other_path.push(next_pp.clone());
+                                            } else {
+                                                ptp.insert(next_p.clone(), vec![next_pp]);
+                                            }
+                                        } else {
+                                            let mut ptp: PathsToPoint = HashMap::new();
+                                            ptp.insert(next_p.clone(), vec![next_pp]);
+                                            queue.insert(next_score, ptp);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else{
+                    break;
+                }
+            };
+            //518
+            Err(String::from("Not solved yet"))
+            // if let Some(min_score) = min_score {
+            //     assert_display(min_score, Some(11048), 85480, "Lowest score", test_mode)
+            // } else {
+            //     Err(String::from("No lowest score foung"))
+            // }
+        } else {
+            Err(String::from("Multiple start or end locations"))
+        }
+    }
+}
+
+pub struct Loc {
+    pos: (isize, isize),
+    d: char,
+    points: usize,
+    path: Vec<(isize, isize)>,
+}
+
+const DIRECTIONS: [(isize, isize); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
+
+pub fn traverse(
+    map: &HashMap<(isize, isize), char>,
+    start: &(isize, isize),
+    end: &(isize, isize),
+) -> (HashMap<((isize, isize), char), usize>, Vec<(isize, isize)>) {
+    let mut visited: HashMap<((isize, isize), char), usize> = HashMap::new();
+    let mut min_points = usize::MAX;
+    let mut paths = Vec::new();
+
+    let mut queue: VecDeque<Loc> = VecDeque::from([Loc {
+        pos: *start,
+        d: '>',
+        points: 0,
+        path: vec![*start],
+    }]);
+
+    while let Some(loc) = queue.pop_front() {
+        if visited
+            .get(&(loc.pos, loc.d))
+            .map_or(false, |&p| p < loc.points)
+        {
+            continue;
+        }
+
+        if loc.pos == *end {
+            if loc.points < min_points {
+                paths = loc.path.clone(); // Reset paths
+                min_points = loc.points;
+            } else if loc.points == min_points {
+                paths.extend(loc.path.clone());
+            }
+        }
+
+        visited.insert((loc.pos, loc.d), loc.points);
+        for &dir in DIRECTIONS.iter() {
+            let new_pos = (loc.pos.0 + dir.0, loc.pos.1 + dir.1);
+            match map[&new_pos] {
+                '#' => continue,
+                '.' => {
+                    let new_d = match dir {
+                        (0, 1) => '>',
+                        (1, 0) => 'v',
+                        (0, -1) => '<',
+                        (-1, 0) => '^',
+                        _ => panic!("Invalid direction"),
+                    };
+
+                    let mut points = loc.points;
+                    if loc.d == new_d {
+                        points += 1;
+                    } else {
+                        points += 1001;
+                    }
+
+                    if points <= *visited.get(&(new_pos, new_d)).unwrap_or(&usize::MAX) {
+                        let mut path = loc.path.clone();
+                        path.push(new_pos);
+
+                        queue.push_back(Loc {
+                            pos: new_pos,
+                            d: new_d,
+                            points,
+                            path,
+                        });
+                    }
+                }
+                _ => panic!("Invalid character"),
+            }
+        }
+    }
+
+    (visited, paths)
 }
