0a0656c778120d4a55b43bde6d931ca03adc9383
Maria Pu <dame.schwinge@gmail.com>
2024-12-23 20:04:49 +0100

    interim

--- advent_of_code/src/advent23.rs
+++ advent_of_code/src/advent23.rs
@@ -1,5 +1,6 @@
-use std::collections::HashSet;
+use std::collections::{BTreeSet, HashMap, HashSet, VecDeque};
 use std::rc::Rc;
+use itertools::Itertools;
 use crate::hashset;
 use crate::utils::{Solve, Label};
 use crate::utils::*;
@@ -20,6 +21,48 @@ impl Default for Advent {
             vertices: HashSet::new(),
             d_edges: HashSet::new()
         }
+    }
+}
+
+impl Advent{
+    #[allow(dead_code)]
+    fn is_disjoint(&self) -> bool{
+        let mut neighbours: VecDeque<Rc<String>> = VecDeque::new();
+        let mut undecided: Vec<Rc<String>> = self.vertices.iter().cloned().collect();
+        if let Some(el) = self.vertices.iter().next(){
+            neighbours.push_back(el.clone());
+        }
+        while let Some(v) = neighbours.pop_front(){
+            let mut undecided_next: Vec<Rc<String>> = Vec::new();
+            while let Some(v_other) = undecided.pop(){
+                if self.d_edges.contains(&(v.clone(), v_other.clone())){
+                    neighbours.push_back(v_other);
+                }
+                else{
+                    undecided_next.push(v_other);
+                }
+            }
+            undecided = undecided_next;
+            if undecided.is_empty(){
+                break
+            }
+        }
+        undecided.len()!=0
+    }
+    fn get_connected_triplets(&self)->HashSet<BTreeSet<Rc<String>>>{
+        let mut q: HashSet<BTreeSet<Rc<String>>> = HashSet::new();
+        for v in self.vertices.iter(){
+            for e in self.edges.iter(){
+                if !e.contains(v) &&
+                    e.iter().all( |v_other|
+                        self.d_edges.contains(&(v.clone(), v_other.clone()))){
+                    let mut vec:BTreeSet<Rc<String>> = e.iter().cloned().collect();
+                    vec.insert(v.clone());
+                    q.insert(vec);
+                }
+            }
+        }
+        q
     }
 }
 
@@ -63,35 +106,77 @@ impl Solve for Advent {
         Ok(())
     }
     fn compute_part1_answer(&self, test_mode: bool) -> Result<String, String>{
-        //super slow
-        //1238
         self.check_input(Some(1))?;
-        let mut result: HashSet<Vec<Rc<String>>> = HashSet::new();
-        for v in self.vertices.iter(){
-            for e in self.edges.iter(){
-                if !e.contains(v) &&
-                    e.iter().all( |v_other|
-                        self.d_edges.contains(&(v.clone(), v_other.clone()))){
-                    let mut vec:Vec<Rc<String>> = e.iter().cloned().collect();
-                    vec.push(v.clone());
-                    vec.sort_unstable();
-                    result.insert(vec);
+        let cnt: usize = self.get_connected_triplets().iter().filter(
+            |&set|{
+                for e in set.iter(){
+                    if e.starts_with('t'){
+                        return true;
+                    }
                 }
+                false
             }
-        }
-        let mut cnt = 0;
-        for set in result.iter(){
-            for e in set.iter(){
-                if e.starts_with('t'){
-                    cnt+=1;
-                    break;
-                }
-            }
-        }
+        ).collect::<Vec<_>>().len();
         assert_display(cnt, Some(7), 1238, "Number of sets", test_mode)
     }
-    // fn compute_part2_answer(&self, test_mode: bool) -> Result<String, String>{
-    //     self.check_input(Some(2))?;
-    //     Err(String::from("Not solved yet"))
-    // }
+    fn compute_part2_answer(&self, test_mode: bool) -> Result<String, String>{
+        self.check_input(Some(2))?;
+        let mut neighbours: HashMap<Rc<String>, HashSet<Rc<String>>> = HashMap::new();
+        for v in self.vertices.iter(){
+            for pair in self.d_edges.iter(){
+                if &(*pair).0 == v{
+                    neighbours.entry(v.clone()).or_insert_with(HashSet::new).insert((*pair).1.clone());
+                }
+            }
+        }
+
+        let mut q = self.get_connected_triplets();
+        loop{
+            let mut q_next: HashSet<BTreeSet<Rc<String>>> = HashSet::new();
+            for e in q.iter() {
+                let mut common: Option<HashSet<Rc<String>>> = None;
+                let mut cont = true;
+                for v in e.iter() {
+                    if !cont{
+                        break;
+                    }
+                    let n = neighbours.get(v).unwrap();
+                    common = match common {
+                        Some(s) => {
+                            if s.is_empty(){
+                                cont = false;
+                            }
+                            Some(s.intersection(n).cloned().collect())
+                        },
+                        None => Some(n.clone())
+                    }
+                }
+                if let Some(common) = common{
+                    for c in common.iter() {
+                        let mut s = e.clone();
+                        s.insert(c.clone());
+                        q_next.insert(s);
+                    }
+                }
+            }
+            if q_next.is_empty(){
+                break;
+            }
+            else{
+                println!("{:?}", &q_next.len());
+                q=q_next;
+            }
+        }
+        if q.len() == 1{
+            let result = vec2line(q.iter().next().unwrap().iter().collect_vec());
+            assert_display(result,
+                           Some(String::from("co,de,ka,ta")),
+                           String::from("bg,bl,ch,fn,fv,gd,jn,kk,lk,pv,rr,tb,vw"),
+                           "Password",
+                           test_mode
+            )
+        }else{
+            Err(String::from("Multiple solutions found"))
+        }
+    }
 }
