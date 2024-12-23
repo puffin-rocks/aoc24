941c09713ce1afeda5c3557eefa7ae41321e375a
Maria Pu <dame.schwinge@gmail.com>
2024-12-24 00:16:14 +0100

    interim

--- advent_of_code/src/advent22.rs
+++ advent_of_code/src/advent22.rs
@@ -1,5 +1,8 @@
+use std::collections::HashMap;
 use crate::advent22::Operation::{Div32, Mult2048, Mult64};
 use crate::utils::{Solve, Label, assert_display};
+use itertools::{izip, Itertools};
+use rayon::prelude::*;
 
 pub(crate) struct Advent {
     label: Label,
@@ -32,6 +35,7 @@ impl Solve for Advent {
     }
     fn compute_part1_answer(&self, test_mode: bool) -> Result<String, String>{
         self.check_input(Some(1))?;
+        return Err(String::from("Skip"));
         let result: usize = self.numbers.iter().map(|n|{
             let mut sn = *n;
             let mut cnt = 0usize;
@@ -41,12 +45,58 @@ impl Solve for Advent {
             }
             sn
         }).sum();
-        assert_display(result, Some(37327623), 18261820068, "Sum of 2000th numbers", test_mode)
+        assert_display(result, Some(37990510), 18261820068, "Sum of 2000th numbers", test_mode)
     }
-    // fn compute_part2_answer(&self, test_mode: bool) -> Result<String, String>{
-    //     self.check_input(Some(2))?;
-    //     Err(String::from("Not solved yet"))
-    // }
+    fn compute_part2_answer(&self, test_mode: bool) -> Result<String, String>{
+    //23: 7+7+9 (-2,1,-1,3)
+        self.check_input(Some(2))?;
+        let n_changes = 2000;
+        let prices_changes: Vec<(Vec<usize>, Vec<isize>)> = self.numbers.par_iter().map(|n|{
+            let mut sn = *n;
+            let mut curr_price = sn%10;
+            let mut prices: Vec<usize> = vec![curr_price];
+            let mut changes: Vec<isize> = Vec::new();
+            let mut cnt = 0usize;
+            while cnt<n_changes{
+                sn = operation(operation(operation(sn,Mult64),Div32),Mult2048);
+                let price = sn%10;
+                changes.push(price as isize - curr_price as isize);
+                curr_price = price;
+                prices.push(curr_price);
+                cnt+=1;
+            }
+            (prices,changes)
+        }).collect();
+        let patterns: Vec<HashMap<[isize;4], usize>> = prices_changes.par_iter()
+            .map(|(prices, changes)|{
+                let mut map: HashMap<[isize;4], usize> = HashMap::new();
+                for (p, pattern) in izip!(prices.windows(4).skip(1), changes.windows(4)){
+                    if !map.contains_key(pattern){
+                        map.insert(<[isize; 4]>::try_from(pattern).unwrap(), p[3]);
+                    }
+                }
+                map
+        }).collect();
+        let mut pattern_cross_count: HashMap<[isize;4], (usize, usize)> = HashMap::new();
+        for m in patterns.iter(){
+            for (pattern, price) in m.iter(){
+                let entry = pattern_cross_count.entry(pattern.clone()).or_insert((0,0));
+                entry.0+=1;
+                entry.1+=*price;
+            }
+        }
+        let mut cnt = 0;
+        for x in pattern_cross_count.iter().sorted_by(|(_,(_,v0)), (_, (_, v1))| v1.cmp(v0)){
+            println!("{:?}", x);
+            cnt+=1;
+            if cnt>10{
+                break
+            }
+        }
+        //println!("{:?}", patterns);
+        //2044
+        Err(String::from("Not solved yet"))
+    }
 }
 
 fn mix(secret_number: usize, component: usize) -> usize {
@@ -54,6 +104,7 @@ fn mix(secret_number: usize, component: usize) -> usize {
 }
 
 fn prune(secret_number: usize) -> usize {
+    //secret_number & ((1<<24) - 1)
     secret_number%16777216
 }
 
@@ -65,9 +116,9 @@ enum Operation{
 
 fn operation(secret_number: usize, operation: Operation)->usize{
     prune(mix(secret_number, match operation{
-        Mult64 => secret_number*64,
-        Div32 => secret_number/32,
-        Mult2048 =>secret_number*2048
+        Mult64 => secret_number<<6,
+        Div32 => secret_number>>5,
+        Mult2048 =>secret_number<<11
     }))
 }
 
