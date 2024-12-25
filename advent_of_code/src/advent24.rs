4275ba9f190712e1f60ee00784d5a393dd42bfed
Maria Pu <dame.schwinge@gmail.com>
2024-12-25 17:09:12 +0100

    AoC24.d24.p2

--- advent_of_code/src/advent24.rs
+++ advent_of_code/src/advent24.rs
@@ -1,41 +1,55 @@
 use std::collections::{BTreeMap, HashMap, VecDeque};
+use std::rc::Rc;
 use itertools::Itertools;
-use crate::utils::{Solve, Label, assert_display};
+use crate::utils::{Solve, Label, assert_display, vec2line};
 
+#[derive(Debug, Clone, Copy, Hash, PartialEq)]
+enum Operator{
+    AND,
+    OR,
+    XOR
+}
+
+impl Eq for Operator {}
 #[derive(Debug, Clone)]
-enum Gate{
-    AND([char;3], [char;3], [char;3]),
-    OR([char;3], [char;3], [char;3]),
-    XOR([char;3], [char;3], [char;3]),
+struct Gate{
+    input1: [char;3],
+    input2: [char;3],
+    output: [char;3],
+    operator: Operator
 }
 
 impl Gate{
+    fn new(input1: [char;3], input2: [char;3], output: [char;3], operator: Operator)->Self{
+        Self{
+            input1,
+            input2,
+            output,
+            operator
+        }
+    }
+    fn write(&self, gate_map: &mut HashMap<([char;3], [char;3], Operator), Rc<Gate>>){
+        let g = Rc::new(self.clone());
+        gate_map.insert((self.input1, self.input2, self.operator), g.clone());
+        gate_map.insert((self.input2, self.input1, self.operator), g.clone());
+    }
     fn evaluate(&self, wires: &mut HashMap<[char;3],bool>)->Result<(),()>{
-        match self{
-            Gate::AND(w1, w2, out ) =>{
-                if let (Some(&w1), Some(&w2)) = (wires.get(w1), wires.get(w2)){
-                    wires.insert(*out, w1 & w2);
-                    Ok(())
-                }else{
-                    Err(())
+        if let (Some(&w1), Some(&w2)) = (wires.get(&self.input1), wires.get(&self.input2)){
+            let value = match &self.operator {
+                Operator::AND => {
+                    w1 & w2
+                },
+                Operator::OR => {
+                    w1 | w2
+                },
+                Operator::XOR => {
+                    w1 ^ w2
                 }
-            },
-            Gate::OR(w1, w2, out )  => {
-                if let (Some(&w1), Some(&w2)) = (wires.get(w1), wires.get(w2)){
-                    wires.insert(*out, w1 | w2);
-                    Ok(())
-                }else{
-                    Err(())
-                }
-            },
-            Gate::XOR(w1, w2, out )  =>{
-                if let (Some(&w1), Some(&w2)) = (wires.get(w1), wires.get(w2)){
-                    wires.insert(*out, w1 ^ w2);
-                    Ok(())
-                }else{
-                    Err(())
-                }
-            }
+            };
+            wires.insert(self.output, value);
+            Ok(())
+        }else{
+            Err(())
         }
     }
 }
@@ -46,11 +60,6 @@ pub(crate) struct Advent {
     gates: Vec<Gate>,
     read_wires: bool
 }
-
-// fn bools_to_binary(bools: &[bool]) -> u32 {
-//     bools.iter().fold(0, |acc, &b| (acc << 1) | (b as u32))
-// }
-
 
 impl Default for Advent {
     fn default() -> Self {
@@ -89,19 +98,18 @@ impl Solve for Advent {
             }else{
                 if let Some((gate_def,out)) = line.split_once(" -> "){
                     if let Some(out) = to_char3(out) {
-                        if let Some((w1, w2)) = gate_def.split_once(" AND ") {
-                            if let (Some(w1), Some(w2)) = (to_char3(w1), to_char3(w2)){
-                                self.gates.push(Gate::AND(w1,w2,out));
-                            }
+                        let (w1, w2, operator) = if let Some((w1, w2)) = gate_def.split_once(" AND ") {
+                            (w1,w2, Operator::AND)
                         } else if let Some((w1, w2)) = gate_def.split_once(" XOR ") {
-                            if let (Some(w1), Some(w2)) = (to_char3(w1), to_char3(w2)){
-                                self.gates.push(Gate::XOR(w1,w2,out));
-                            }
+                            (w1,w2, Operator::XOR)
                         }
                         else if let Some((w1, w2)) = gate_def.split_once(" OR ") {
-                            if let (Some(w1), Some(w2)) = (to_char3(w1), to_char3(w2)){
-                                self.gates.push(Gate::OR(w1,w2,out));
-                            }
+                            (w1,w2, Operator::OR)
+                        }else{
+                            unreachable!()
+                        };
+                        if let (Some(w1), Some(w2)) = (to_char3(w1), to_char3(w2)){
+                            self.gates.push(Gate::new(w1,w2,out, operator));
                         }
                     }
                 }
@@ -139,8 +147,105 @@ impl Solve for Advent {
             .values().rev().fold(0, |acc, &b| (acc << 1) | (b as usize));
         assert_display(result, Some(2024), 50411513338638, "Z-binary output", test_mode)
     }
-    // fn compute_part2_answer(&self, test_mode: bool) -> Result<String, String>{
-    //     self.check_input(Some(2))?;
-    //     Err(String::from("Not solved yet"))
-    // }
+    fn compute_part2_answer(&self, test_mode: bool) -> Result<String, String>{
+        self.check_input(Some(2))?;
+        let width = self.wires.len()/2;
+        let mut gate_map: HashMap<([char;3], [char;3], Operator), Rc<Gate>> = HashMap::new();
+        for g in self.gates.iter(){
+            g.write(&mut gate_map);
+        }
+        let mut wires = self.wires.clone();
+        let mut to_swap: Vec<String> = Vec::new();
+        while let Some((g1, g2)) = check(&gate_map, &mut wires, width){
+            to_swap.push(g1.output.iter().join(""));
+            to_swap.push(g2.output.iter().join(""));
+            gate_map.insert((g1.input1, g1.input2, g1.operator), g1.clone());
+            gate_map.insert((g1.input2, g1.input1, g1.operator), g1.clone());
+            gate_map.insert((g2.input1, g2.input2, g2.operator), g2.clone());
+            gate_map.insert((g2.input2, g2.input1, g2.operator), g2.clone());
+        }
+        to_swap.sort_unstable();
+        assert_display(vec2line(to_swap),None, String::from("gfv,hcm,kfs,tqm,vwr,z06,z11,z16"), "Wires to swap", test_mode)
+    }
+}
+
+fn check(gate_map: &HashMap<([char;3], [char;3], Operator), Rc<Gate>>,
+         wires: &mut HashMap<[char; 3], bool>, width:usize)->Option<(Rc<Gate>,Rc<Gate>)>{
+    let mut curr_position = 0;
+    let mut carry;
+    if let (Some(z), Some(c)) = (gate_map.get(&(['x','0','0'], ['y','0','0'], Operator::XOR)),
+                                 gate_map.get(&(['x','0','0'], ['y','0','0'], Operator::AND))){
+        if let (Ok(_), Ok(_)) = ((*z).evaluate(wires), (*c).evaluate(wires)) {
+            let key1 = to_char3(format!("{}{:02}", 'z', curr_position).as_str()).unwrap();
+            let value1 = wires.get(&key1);
+            let key2 = z.output;
+            let value2 = wires.get(&key2);
+            if value1!=value2{
+                panic!("{:?}", (key1, key2));
+            }
+            carry = c.output;
+            curr_position+=1;
+        }else{
+            unreachable!();
+        }
+    }else{
+        return None;
+    };
+    while curr_position<width{
+        let x = to_char3(format!("{}{:02}", 'x', curr_position).as_str()).unwrap();
+        let y = to_char3(format!("{}{:02}", 'y', curr_position).as_str()).unwrap();
+        if let (Some(v), Some(t)) = (gate_map.get(&(x, y, Operator::XOR)),
+                                     gate_map.get(&(x, y, Operator::AND))){
+            match (gate_map.get(&(v.output, carry, Operator::XOR)),
+                   gate_map.get(&(v.output, carry, Operator::AND))){
+                (Some(z), Some(f)) => {
+                    match gate_map.get(&(f.output, t.output, Operator::OR)) {
+                        Some(c) => {
+                            if let (Ok(_), Ok(_), Ok(_), Ok(_), Ok(_)) = ((*v).evaluate(wires),
+                                                                          (*t).evaluate(wires),
+                                                                          (*z).evaluate(wires),
+                                                                          (*f).evaluate(wires),
+                                                                          (*c).evaluate(wires)) {
+                                let key1 = to_char3(format!("{}{:02}", 'z', curr_position).as_str()).unwrap();
+                                let value1 = wires.get(&key1);
+                                let key2 = z.output;
+                                let value2 = wires.get(&key2);
+                                if value1 != value2 {
+                                    let g_c = Rc::new(Gate::new(f.output, t.output, z.output, Operator::OR));
+                                    let g_z = Rc::new(Gate::new(v.output, carry, c.output, Operator::XOR));
+                                    return Some((g_c, g_z))
+                                }
+                                carry = c.output;
+                                curr_position += 1;
+                            } else {
+                                unreachable!();
+                            }
+                        },
+                        None => {
+
+                            if let Some(_) = gate_map.get(&(f.output, z.output, Operator::OR)) {
+                                let g_t = Rc::new(Gate::new(x, y, z.output, Operator::AND));
+                                let g_z = Rc::new(Gate::new(v.output, carry, t.output, Operator::XOR));
+                                return Some((g_t, g_z))
+                            } else if let Some(_) = gate_map.get(&(t.output, z.output, Operator::OR)) {
+                                let g_f = Rc::new(Gate::new(v.output, carry, z.output, Operator::AND));
+                                let g_z = Rc::new(Gate::new(v.output, carry, f.output, Operator::XOR));
+                                return Some((g_f, g_z))
+                            } else {
+                                panic!("Not found {:?}", (f.output, t.output, curr_position));
+                            }
+                        }
+                    }
+                },
+                _ =>{
+                    let g_t = Rc::new(Gate::new(x, y, v.output, Operator::AND));
+                    let g_v = Rc::new(Gate::new(x, y, t.output, Operator::XOR));
+                    return Some((g_v, g_t));
+                }
+            }
+        } else {
+            println!("{:?}", (x, y));
+        }
+    }
+    None
 }
